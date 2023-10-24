e748a4ef51de1daf1965a82a12a885c75dc80646
yuvrajpalariya <132062719+yuvrajpalariya@users.noreply.github.com>
2023-10-24 13:13:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+
+<script>
+	const submitBtn = document.getElementById('submit')
+const closeBtn = document.getElementById('close')
+const popup = document.querySelector('.popupContainer')
+
+submitBtn.addEventListener('click', function () {
+    popup.classList.add('popupActive')
+
+})
+closeBtn.addEventListener('click', function () {
+    popup.classList.remove('popupActive')
+
+})
+</script>
+
+
+
+<script>
+$(document).ready(function(){
+$("#flip").click(function(){
+$("#panel").slideToggle("slow");
+
+});
+});
+</script>
+
+
+
+
+<script>
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+</script>
